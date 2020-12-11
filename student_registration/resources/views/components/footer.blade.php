c6ee3fa90bec7c3d5f198a467f646654289fe4b8
gabrielzona23 <gabriel.zonatto@hotmail.com>
2020-12-11 11:11:02 -0500

    (added) components and views initiais

---
+++ student_registration/resources/views/components/footer.blade.php
@@ -0,0 +1,25 @@
+<div>
+    <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
+    <div class="flex-grow-1"></div>
+    <div class="app-footer">
+        <div class="row">
+            <div class="col-md-9">
+                <p><strong>Sistema Web de cadastro de alunos da Centro de ensino especial Dom Bosco</strong></p>
+                <p>Aqui vai os nossos blabla
+                    <sunt></sunt>
+                </p>
+            </div>
+        </div>
+        <div class="footer-bottom border-top pt-3 d-flex flex-column flex-sm-row align-items-center">
+            <a class="btn btn-primary text-white btn-rounded" href="" target="_blank">Sistemas de Informação UFAC</a>
+            <span class="flex-grow-1"></span>
+            <div class="d-flex align-items-center">
+                <img class="logo" src="{{asset('images/logo.png')}}" alt="">
+                <div>
+                    <p class="m-0">&copy; 2020  Gabriel & Natália</p>
+                    <p class="m-0">Todos os direitos reservados</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
