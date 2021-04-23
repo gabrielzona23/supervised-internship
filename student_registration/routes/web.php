36394ddbddffd6e08c0ea9d2ac9ba0271d037e01
gabrielzona23 <gabriel.zonatto@hotmail.com>
2021-04-23 10:37:35 -0500

    add: add files config

--- student_registration/routes/web.php
+++ student_registration/routes/web.php
@@ -1,22 +1,24 @@
 <?php
 
-use App\Http\Controllers\AddressController;
-use App\Http\Controllers\RegistrationController;
-use App\Http\Controllers\ResponsiblyController;
-use App\Http\Controllers\StudentController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
-    return view('layouts.home');
-});
-Route::get('/foo', function () {
-    return 'Hello World';
+    return view('welcome');
 });
 
-Route::resource('users', UserController::class);
-Route::resource('students', StudentController::class);
-Route::resource('addresses', AddressController::class);
-Route::resource('responsiblies', ResponsiblyController::class);
-Route::resource('registrations', RegistrationController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
